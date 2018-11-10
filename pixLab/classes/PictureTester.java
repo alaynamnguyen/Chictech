ceb1779a1e8cc9e4be353aa6522510dae3f504b0
Isa Dash <anchald2@illinois.edu>
2018-11-10 11:44:24 -0600

    Update PictureTester.java

--- pixLab/classes/PictureTester.java
+++ pixLab/classes/PictureTester.java
@@ -1,4 +1,30 @@
 public class PictureTester
 {
- 
+  
+	public static void testPhotoshop()
+  {
+	  Picture pisa = new Picture("Himalayan (2).jpg");
+	  Picture caitlyn = new Picture("thicc.jpg");
+	  
+	  pisa.Photoshop(caitlyn);
+	  caitlyn.explore();
+	  pisa.explore();
+	  caitlyn.explore();
+  }
+	
+  public static void testGrayScale() {
+  	Picture pisa = new Picture("Himalayan (2).jpg");
+    pisa.grayscale();
+    pisa.explore();
+  	
+  	
+  }
+
+  /** Main method for testing.  Every class can have a main
+    * method in Java */
+  public static void main(String[] args)
+  {
+    //testPhotoshop();
+    testGrayScale();
+  }
 }
